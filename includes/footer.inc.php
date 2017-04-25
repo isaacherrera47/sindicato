634f2958f5bd11bbf1332246b928cdbae97dda67
Isaac Herrera <isaacherrera47@gmail.com>
2017-04-24 20:35:29 -0700

    ! separated footer and header.

---
+++ includes/footer.inc.php
@@ -0,0 +1,20 @@
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.ba-cond.min.js"></script>
+<script src="js/jquery.slitslider.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+<script src="js/datepicker.min.js"></script>
+<script src="js/datepicker.en.min.js"></script>
+<script src="js/timepicki.min.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/smooth.scroll.min.js"></script>
+<script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC0dSWcBx-VghzhzQB6oCMTgeXMOhCYTvk"></script>
+<script src="js/map.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -46,3 +46,27 @@ $site = new Resource();
     <script type="text/javascript" src="js/modernizr.custom.79639.min.js"></script>
 </head>
 <body>
+
+<div class="page-holder">
+    <!-- Navbar -->
+    <header class="header">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="/Sindicato/" class="navbar-brand"><img src="img/logo.png" width="100"></a>
+                    <div class="navbar-buttons">
+                        <button type="button" data-toggle="collapse" data-target=".navbar-collapse"
+                                class="navbar-toggle navbar-btn">Menu<i class="fa fa-align-justify"></i></button>
+                    </div>
+                </div>
+                <div id="navigation" class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#top">Inicio</a></li>
+                        <li><a href="#about">Acerca de</a></li>
+                        <li><a href="#contact">Contacto</a></li>
+                    </ul>
+                    <a href="#" class="btn navbar-btn btn-unique hidden-sm hidden-xs" id="open-reservation">Editorial SUTITSPR</a>
+                </div>
+            </div>
+        </nav>
+    </header><!-- End Navbar -->
